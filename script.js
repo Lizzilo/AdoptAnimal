398266b734a131d3236cd5c12171df1d45636bc2
Liza Odish <lizaodishl@gmail.com>
2023-05-15 22:56:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const searchForm = document.getElementById('search-form');
 const resultsContainer = document.getElementById('results');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const animalType = document.getElementById('animal-type').ariaValueMax;
+    fetchAnimals(animalType);
+});
+
+function fetchAnimals(animalType) {
+    resultsContainer.innerHTML = '';
+
+    fetch('https://api.adoptapet.com/search/pets_at_shelters?') {
+        
+        }
+    }
+}
